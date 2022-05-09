81c68418ce28dffd255042e867ac023900d84c78
berryberry189 <berryberry189@naver.com>
2022-05-09 21:19:06 +0900

    예제 1

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class) // 스프인 빈 등록
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스텐 범위 지정
 public class ProxyApplication {
 
 	public static void main(String[] args) {
