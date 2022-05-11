0dfdb2303587f1aa192a1e80766c687bae650bac
berryberry189 <berryberry189@naver.com>
2022-05-11 22:17:00 +0900

    예제 2

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) // 스프인 빈 등록
+// @Import(AppV1Config.class) // 스프인 빈 등록
+@Import(AppV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스텐 범위 지정
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping
+@ResponseBody // @Controller 는 component scan 의 대상이 되므로 여기서는 사용 X
+public class OrderControllerV2{
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
