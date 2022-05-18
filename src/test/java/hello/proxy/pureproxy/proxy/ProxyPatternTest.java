b8d70937b543300ac4ef48999b30ebf8814855da
berryberry189 <berryberry189@naver.com>
2022-05-18 22:59:18 +0900

    프록시 패턴 예제코드2

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,17 @@ public class ProxyPatternTest {
     void noProxyTest(){
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest(){
+        // client -> cacheProxy -> realSubject 런타임 객체 의존관계 완성
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
