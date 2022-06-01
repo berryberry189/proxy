26d3a98081e239173409bdf49283b25406ea0f2c
berryberry189 <berryberry189@naver.com>
2022-06-01 21:09:10 +0900

    인터페이스 기반 프록시 vs 클래스 기반 프록시

--- src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
+++ src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -16,6 +16,14 @@ public class InterfaceProxyConfig {
     // => 따라서 프록시 객체는 스프링 컨테이너가 관리하고 자바 힙메모리에도 올라간다.
     // 반면 실제 객체는 힙 메모리에는 올라가지만 스프링 컨테이너가 관리하지는 않는다.
 
+
+    // 추가 설명
+    // 이론적으로는 모든 객체에 인터페이스를 도입해서 역할과 구현을 나누는 것이 좋지만,
+    // 실제로는 구현을 거의 변경할 일이 없는 클래스도 많다.
+    // 구현을 변경할 일이 없는 코드에 무작정 인터페이스를 사용하는 것은 번거롭고 실용적이지 않으므로 이런 경우에는 구체 클래스를 바로 사용하는 것이 좋다.
+    // 인터페이스가 있는 경우 & 구체클래스가 있는 경우 두가지 상황을 모두 대응할 수 있어야 한다.
+
+
     @Bean // 빈 이름 : orderController, 빈 객체 : OrderControllerInterfaceProxy
     public OrderControllerV1 orderController(LogTrace logTrace){
         OrderControllerV1Impl orderControllerImpl = new OrderControllerV1Impl(orderService(logTrace));
