686a7320adaaee92b4d0478966d0c66f8337fddf
berryberry189 <berryberry189@naver.com>
2022-06-17 23:33:20 +0900

    동적 프록시 기술 주석 추가

--- src/test/java/hello/proxy/cglib/CglibTest.java
+++ src/test/java/hello/proxy/cglib/CglibTest.java
@@ -9,6 +9,14 @@ import org.springframework.cglib.proxy.Enhancer;
 @Slf4j
 public class CglibTest {
 
+    // 리플렉션 - 메타 데이터
+    // jdk 동적 프록시 - 직접 프록시를 만들지 않고 실시간으로 동적으로 프록시를 만들어냄 (InvocationHandler 를 통해)
+        // 인터페이스 사용
+        // 클라이언트 -> 프록시 호출 -> 인보케이션 핸들러 호출 -> 핸들러가 알아서 구현체 호출
+    // cglib - 바이트 코드를 조작해서 동적으로 클래스를 생성하는 기술을 제공
+        // 인터페이스 없이 구체클래스만 가지고 동적 프록시 생성 가능
+
+
     @Test
     void cglib(){
         ConcreteService target = new ConcreteService();
